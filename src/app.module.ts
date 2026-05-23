5607947fb0fd6cb01d0e54301d1a7eeaaa1ba0cf
Anunay Argha <adobex092@gmail.com>
2026-05-23 10:00:38 +0600

    test(prisma): Test prisma data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersService } from './user.service';
+import { PrismaService } from './prisma.service';
+import { PostsService } from './post.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService, PostsService, PrismaService],
 })
 export class AppModule {}
